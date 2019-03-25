a0408c6ebaa64de95f3148532943670adc57bcc6
appleater7 <appleater7@gmail.com>
2019-03-25 18:45:21 +0900

    3.25

--- src/com/osf/test/servlet/FoodServlet.java
+++ src/com/osf/test/servlet/FoodServlet.java
@@ -1,41 +1,64 @@
 package com.osf.test.servlet;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FoodSevlet
- */
-@WebServlet("/FoodSevlet")
+import org.apache.commons.beanutils.BeanUtils;
+
+import com.osf.test.service.FoodService;
+import com.osf.test.service.impl.FoodServiceImpl;
+import com.osf.test.utils.URICommand;
+import com.osf.test.vo.Food;
+
 public class FoodServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FoodServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static FoodService fs = new FoodServiceImpl();
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String cmd = URICommand.getCommand(request);
+		Food f = new Food();
+		try {
+			BeanUtils.populate(f, request.getParameterMap());
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		if ("food".equals(cmd)) {
+			System.out.println(fs.selectFood(f.getFoodNum()));
+		}else if ("foods".equals(cmd)) {
+			System.out.println(fs.selectFoodList());
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		request.setCharacterEncoding("utf-8");
+		String cmd = request.getParameter("cmd");
+		Food f = new Food();
+		try {
+			BeanUtils.populate(f, request.getParameterMap());
+		} catch (IllegalAccessException e) {
 
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+
+			e.printStackTrace();
+		}
+		
+		System.out.println("cmd : " + cmd);
+		System.out.println(f);
+		if ("insert".equals(cmd)) {
+			System.out.println("insert : " + fs.insertFood(f));
+		}else if ("update".equals(cmd)) {
+			System.out.println("update : " + fs.updateFood(f));
+		}else if ("delete".equals(cmd)) {
+			System.out.println("delete : " + fs.deleteFood(f.getFoodNum()));
+		}
+	}
 }
---
+++ src/com/osf/test/test/ReflectionTest.java
@@ -0,0 +1,21 @@
+package com.osf.test.test;
+
+class Test {
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+	public void setTest(String test) {
+		this.test = test;
+	}
+	@Override
+	public String toString() {
+		return "Test [test=" + test + "]";
+	}	
+}
+public class ReflectionTest {	
+	public static void main(String[] args) {
+		
+	}
+}
